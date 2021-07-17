2f34b45bdb436ccfcf2bfe43df472c4161e99a97
Fangwei Hsu <>
2021-07-17 11:38:01 +0900

    create a project

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '14.0'
+
+target 'Recipe' do
+  use_frameworks!
+  pod 'Alamofire', '~> 5.4'
+  pod 'RxSwift', '6.2.0'
+  pod 'RxCocoa', '6.2.0'
+end
---
+++ Recipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipe
+//
+//  Created by Fangwei Hsu on 2021/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
